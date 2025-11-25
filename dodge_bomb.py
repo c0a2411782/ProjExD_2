2516495dc86755848617af39d729a37dbcc89c3e
tatsuki <c0a2411782@edu.teu.ac.jp>
2025-11-25 14:42:12 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -50,6 +50,10 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+
+        if kk_rct.colliderect(bb_rct):
+            print("game over")
+            return
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         # if key_lst[pg.K_UP]:
@@ -77,6 +81,7 @@ def main():
              vy *= -1
         bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
